3179ae5a9ba31411ebb65975df46b87782e1149e
Makhmut <mahmudlyder@gmail.com>
2022-10-09 13:59:47 +0600

    feat: update manifest py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Open Academy",
-
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
@@ -9,26 +8,17 @@
     'description': """
         Long description of module's purpose
     """,
-
     'author': "Makhmut",
     'website': "",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Extra',
+    'category': 'Productivity',
+    'license': 'LGPL-3',
     'version': '0.1',
+    'depends': [],
 
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
