ed3bc4243026b1b3874acdbffb011e928c2cc0c1
Makhmut <mahmudlyder@gmail.com>
2022-10-09 14:01:55 +0600

    feat: add instructor model

--- models/models.py
+++ models/models.py
@@ -9,3 +9,19 @@ class Course(models.Model):
 
     name = fields.Char(string ='title', required=True)
     description = fields.Text()
+    instructor_id = fields.Many2one('openacademy.instructor', 
+        string="Instructor", required = True)
+
+
+class Instructor(models.Model):
+    _name = 'openacademy.instructor'
+    _description = 'Open Academy instructor'
+
+    name = fields.Char(string='Name', required=True)
+    age = fields.Integer(string='Age')
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'other'),
+    ], required=True, default='male')
+    note = fields.Text(string='Description')
